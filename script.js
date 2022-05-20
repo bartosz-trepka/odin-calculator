3a13ecbbe08c697beb05d3aaf6040a8ed01082c4
Bartosz Trepka <bartosz.trepka@gmail.com>
2022-05-20 15:24:53 +0200

    Update script.js - function operate()

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return b !== 0 ? a / b : 'ERROR';
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
